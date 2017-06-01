836169fba633467fdee1ea339a4680631c0220e2
Leo Andres <lmandres@yahoo.com>
2017-06-01 18:15:18 -0400

    [BattleHex master 2017-06-01 16-54] Committing changes after adding code to communicate through Firebase Messaging.  Worked on server code as well.

--- src/main/java/net/andresi/battlehex/GameNotifierServlet.java
+++ src/main/java/net/andresi/battlehex/GameNotifierServlet.java
@@ -12,6 +12,9 @@ import javax.servlet.http.*;
 import javax.net.ssl.HttpsURLConnection;
 
 public class GameNotifierServlet extends HttpServlet {
+
+	static final String AUTH_KEY = "AAAAWaB3uw0:APA91bFq04mXdoMaiLN0hNEF6KZvsdpW8UKKVGrKaVNST7sqFGDBXB4bdeQFFlwqujrROsGWGGyxmBZJ0saDh0Ou5JPdeeRntgS9pbhShN8W12d7P95Txp6aO8itTOW8y5USreaQMcVH";
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, World!");
@@ -23,6 +26,10 @@ public class GameNotifierServlet extends HttpServlet {
 
 		URL urlObj = new URL(url);
 		HttpsURLConnection con = (HttpsURLConnection)urlObj.openConnection();
+
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "application/json");
+		con.setRequestProperty("Authorization", "key=" + AUTH_KEY);
 	}
 }
 
